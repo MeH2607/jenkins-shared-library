06aa92ad4dc8bef40686a855a0f76253e190c978
Mohamed El Haddaoui <113069009+MeH2607@users.noreply.github.com>
2024-09-05 22:01:36 +0200

    Update runChildPipeline.groovy

--- vars/runChildPipeline.groovy
+++ vars/runChildPipeline.groovy
@@ -1,1 +1,5 @@
-
+// I din Shared Library (vars/runChildPipeline.groovy)
+def call(String childJobName, Map params = [:]) {
+    def result = build job: childJobName, parameters: params.collect { k, v -> string(name: k, value: v) }, propagate: false
+    return result.getBuildVariables() // Henter environment-variabler fra child pipeline
+}

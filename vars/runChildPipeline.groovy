c0720d32edb99c8f86420cc2af7eee41b652153f
Mohamed El Haddaoui <113069009+MeH2607@users.noreply.github.com>
2024-09-06 12:53:46 +0200

    Update runChildPipeline.groovy

--- vars/runChildPipeline.groovy
+++ vars/runChildPipeline.groovy
@@ -1,6 +1,4 @@
 def call(String childJobName, Map params = [:]) {
-    // Kør child pipeline uden at returnere build variabler
     def result = build job: childJobName, parameters: params.collect { k, v -> string(name: k, value: v) }, propagate: false
-    // Returner ikke build variabler her, da parent pipeline håndterer artifacts
     return result
 }
